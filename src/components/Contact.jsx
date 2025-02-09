4f134718052112377aec1737a6c0421b1f92bf19
vladik-Didyk <vladik.didyk@GMAIL.COM>
2025-02-09 17:58:05 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
   };
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+    <div className={`xl:mt-12 flex justify-center xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>{TEXT.sectionSubText}</p>
         <h3 className={styles.sectionHeadText}>{TEXT.sectionHeadText}</h3>
